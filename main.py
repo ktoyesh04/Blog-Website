c62cfbfec980c6b07659e0f46fa2dcaecaaadd32
Kurapati Toyesh <124575344+ktoyesh04@users.noreply.github.com>
2024-02-05 00:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+# db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
